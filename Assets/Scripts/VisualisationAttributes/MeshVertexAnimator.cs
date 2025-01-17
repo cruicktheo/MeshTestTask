1c4d41a578330d18c325c7279c509ef382067477
ted <ted@tedlistens.com>
2025-01-17 23:28:59 +0000

    Add mesh vertex animation script

---
+++ Assets/Scripts/VisualisationAttributes/MeshVertexAnimator.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+namespace MeshTestTask
+{
+    public class MeshVertexAnimator : VisualisationAttribute
+    {
+        private Mesh mesh;
+        private Vector3[] originalVertices;
+
+        public MeshVertexAnimator(Mesh mesh)
+        {
+            this.mesh = mesh;
+            Events.OnToggleMeshVertexAnimation += OnToggled;
+        }
+
+        public override void Update()
+        {
+            if (IsActive)
+            {
+                SetVertexNoise();
+            }
+        }
+
+        public override void Cleanup()
+        {
+            Events.OnToggleMeshVertexAnimation -= OnToggled;
+        }
+
+        private void SetVertexNoise()
+        {
+            var vertices = new Vector3[mesh.vertexCount];
+
+            for (var i = 0; i < vertices.Length; i++)
+            {
+                var vertex = originalVertices[i];
+                var noiseX = vertex.x * 1f + Time.time * 1f;
+                var noiseY = vertex.y * 1f + Time.time * 1f;
+
+                vertices[i] = vertex + mesh.normals[i] * Mathf.PerlinNoise(noiseX, noiseY) * 0.5f;
+            }
+
+            mesh.vertices = vertices;
+        }
+
+        private void OnToggled()
+        {
+            if (IsActive)
+            {
+                mesh.vertices = originalVertices;
+            }
+            else
+            {
+                originalVertices = (Vector3[])mesh.vertices.Clone();
+            }
+
+            ToggleAttributeActive();
+        }
+    }
+}
