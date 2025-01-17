2652870152685d076b0fde32598ee3ca34e7616d
ted <ted@tedlistens.com>
2025-01-17 18:14:20 +0000

    Add scripts for UI

---
+++ Assets/Scripts/UI/HUDPanel.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+    
+namespace MeshTestTask
+{
+    public class HUDPanel : MonoBehaviour
+    {
+        public void OnToggleLissajousAnimatorPressed()
+        {
+            Events.OnToggleLissajousAnimator?.Invoke();
+        }
+
+        public void OnToggleMeshVertexAnimationPressed()
+        {
+            Events.OnToggleMeshVertexAnimation?.Invoke();
+        }
+
+        public void OnToggleObjectRotatorPressed()
+        {
+            Events.OnToggleObjectRotator?.Invoke();
+        }
+
+        public void OnToggleRotationalColorPressed()
+        {
+            Events.OnToggleRotationalColor?.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/UserFollow.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+namespace MeshTestTask
+{
+    public class UserFollow : MonoBehaviour
+    {
+        private const float EASE_TIME = 0.1f;
+        private const float ROTATION_THRESHOLD_DEGREES = 45f;
+        private const float USER_CENTER_DISTANCE_THRESHOLD = 2f;
+        [SerializeField] private Transform user;
+        private FloatCriticalDamper rotationDamper;
+        private Vector3CriticalDamper positionDamper;
+
+        private void Start()
+        {
+            rotationDamper = new FloatCriticalDamper();
+            positionDamper = new Vector3CriticalDamper();
+            rotationDamper.SetEaseTime(EASE_TIME);
+            positionDamper.SetEaseTime(EASE_TIME);
+        }
+
+        private void Update()
+        {
+            CheckRotation();
+            CheckPosition();
+        }
+
+        private void CheckRotation()
+        {
+            var userXRotation = user.transform.rotation.eulerAngles.y.ClampToSigned180DegreeRange();
+            var thisXRotation = transform.rotation.eulerAngles.y.ClampToSigned180DegreeRange();
+
+            if (Mathf.Abs(userXRotation - thisXRotation) > ROTATION_THRESHOLD_DEGREES)
+            {
+                rotationDamper.SetTargetValue(userXRotation);
+            }
+
+            UpdateRotation();
+        }
+
+        private void CheckPosition()
+        {
+            var userPosition = user.position;
+            var thisPosition = transform.position;
+
+            // Flatten height so that it doesnt recorrect when moving up or down
+            userPosition.y = 0f;
+            thisPosition.y = 0f;
+
+            if (Vector3.Distance(thisPosition, userPosition) > USER_CENTER_DISTANCE_THRESHOLD)
+            {
+                positionDamper.SetTargetValue(user.position);
+            }
+            
+            UpdatePosition();
+        }
+
+        private void UpdateRotation()
+        {
+            rotationDamper.Update(Time.deltaTime);
+            transform.rotation = Quaternion.Euler(new Vector3(0f, rotationDamper.GetCurrentValue(), 0f));
+        }
+
+        private void UpdatePosition()
+        {
+            positionDamper.Update(Time.deltaTime);
+            transform.position = positionDamper.GetCurrentValue();
+        }
+    }
+}
