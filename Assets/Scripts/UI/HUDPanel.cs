7b1a1fc42584519bc0bd88b8544281e46ac49fff
ted <ted@tedlistens.com>
2025-01-17 23:27:00 +0000

    Add UI

--- Assets/Scripts/UI/HUDPanel.cs
+++ Assets/Scripts/UI/HUDPanel.cs
@@ -1,27 +1,52 @@
 ﻿using UnityEngine;
-    
+using UnityEngine.UI;    
+
 namespace MeshTestTask
 {
     public class HUDPanel : MonoBehaviour
     {
+        private readonly Color ButtonOnColor = Color.green;
+        private readonly Color ButtonOffColor = Color.white;
+        [SerializeField] private Image lissajousAnimatorButton;
+        [SerializeField] private Image meshAnimationButton;
+        [SerializeField] private Image objectRotatorButton;
+        [SerializeField] private Image rotationalColorButton;
+        private bool lissajousButtonOn;
+        private bool meshAnimationButtonOn;
+        private bool objectRotatorButtonOn;
+        private bool rotationalColorButtonOn;
+
         public void OnToggleLissajousAnimatorPressed()
         {
+            lissajousButtonOn = !lissajousButtonOn;
+            SetButtonColorFromStatus(lissajousAnimatorButton, lissajousButtonOn);
             Events.OnToggleLissajousAnimator?.Invoke();
         }
 
         public void OnToggleMeshVertexAnimationPressed()
         {
+            meshAnimationButtonOn = !meshAnimationButtonOn;
+            SetButtonColorFromStatus(meshAnimationButton, meshAnimationButtonOn);
             Events.OnToggleMeshVertexAnimation?.Invoke();
         }
 
         public void OnToggleObjectRotatorPressed()
         {
+            objectRotatorButtonOn = !objectRotatorButtonOn;
+            SetButtonColorFromStatus(objectRotatorButton, objectRotatorButtonOn);
             Events.OnToggleObjectRotator?.Invoke();
         }
 
         public void OnToggleRotationalColorPressed()
         {
+            rotationalColorButtonOn = !rotationalColorButtonOn;
+            SetButtonColorFromStatus(rotationalColorButton, rotationalColorButtonOn);
             Events.OnToggleRotationalColor?.Invoke();
+        }
+
+        private void SetButtonColorFromStatus(Image button, bool isOn)
+        {
+            button.color = isOn ? ButtonOnColor : ButtonOffColor;
         }
     }
 }
---
+++ Assets/Scripts/UI/LissajousPanel.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using TMPro;
+
+namespace MeshTestTask
+{
+    public class LissajousPanel : MonoBehaviour
+    {
+        private const float FLOAT_STEP = 0.1f;
+        private const string FLOAT_STRING_FORMAT = "N1";
+        [SerializeField] GameObject root;
+        [SerializeField] private LissajousSettings settings;
+        [SerializeField] private TMP_Text AValue;
+        [SerializeField] private TMP_Text BValue;
+        [SerializeField] private TMP_Text aValue;
+        [SerializeField] private TMP_Text bValue;
+        [SerializeField] private TMP_Text deltaValue;
+
+        private void Start()
+        {
+            SetValueDisplays();
+        }
+
+        private void OnEnable()
+        {
+            Events.OnToggleLissajousAnimator += ToggleShowRoot;
+        }
+
+        private void OnDisable()
+        {
+            Events.OnToggleLissajousAnimator += ToggleShowRoot;
+        }
+
+        public void OnADecremented()
+        {
+            settings.A--;
+            SetValueDisplays();
+        }
+
+        public void OnBDecremented()
+        {
+            settings.B--;
+            SetValueDisplays();
+        }
+
+        public void OnaDecremented()
+        {
+            settings.a--;
+            SetValueDisplays();
+        }
+
+        public void OnbDecremented()
+        {
+            settings.b--;
+            SetValueDisplays();
+        }
+
+        public void OnDeltaDecremented()
+        {
+            settings.delta -= FLOAT_STEP;
+            SetValueDisplays();
+        }
+
+        public void OnAIncremented()
+        {
+            settings.A++;
+            SetValueDisplays();
+        }
+
+        public void OnBIncremented()
+        {
+            settings.B++;
+            SetValueDisplays();
+        }
+
+        public void OnaIncremented()
+        {
+            settings.a++;
+            SetValueDisplays();
+        }
+
+        public void OnbIncremented()
+        {
+            settings.b++;
+            SetValueDisplays();
+        }
+
+        public void OnDeltaIncremented()
+        {
+            settings.delta += FLOAT_STEP;
+            SetValueDisplays();
+        }
+
+        private void SetValueDisplays()
+        {
+            AValue.text = settings.A.ToString();
+            BValue.text = settings.B.ToString();
+            aValue.text = settings.a.ToString();
+            bValue.text = settings.b.ToString();
+            deltaValue.text = settings.delta.ToString(FLOAT_STRING_FORMAT);
+        }
+
+        private void ToggleShowRoot()
+        {
+            root.SetActive(!root.activeSelf);
+        }
+    }
+}
